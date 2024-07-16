5075c6775cd70c0e68d4d867c9e81274e26c0d57
amir hossein <a.hosseinkarimi82@gmail.com>
2024-07-16 11:41:15 +0330

    fix: changes

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
   <p align="center">
     <a href="https://www.npmjs.com/package/react-native-app-tour"><img src="http://img.shields.io/npm/v/react-native-app-tour.svg?style=flat" /></a>
-    <a href="https://github.com/prscX/react-native-app-tour/pulls"><img alt="PRs Welcome" src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg" /></a>
-    <a href="https://github.com/prscX/react-native-app-tour#License"><img src="https://img.shields.io/npm/l/react-native-app-tour.svg?style=flat" /></a>
+    <a href="https://github.com/amir-hossein-karimi/react-native-app-tour/pulls"><img alt="PRs Welcome" src="https://img.shields.io/badge/PRs-welcome-brightgreen.svg" /></a>
+    <a href="https://github.com/amir-hossein-karimi/react-native-app-tour#License"><img src="https://img.shields.io/npm/l/react-native-app-tour.svg?style=flat" /></a>
   </p>
 
     ReactNative: Native App Tour Library (Android/iOS)
@@ -84,7 +84,7 @@ allprojects {
     - FIX: remove react-native-app-tour from package.json and rerun
   - Pods version is out of date.
     - `pod repo update`
-- If you encounter `File not found in iOS` issue while setup, please refer [ISSUE - 3](https://github.com/prscX/react-native-app-tour/issues/3) issue which might help you in order to resolve.
+- If you encounter `File not found in iOS` issue while setup, please refer [ISSUE - 3](https://github.com/amir-hossein-karimi/react-native-app-tour/issues/3) issue which might help you in order to resolve.
 - If you have problems with `Android` Trying to resolve view with tag which doesn't exist or can't resolve tag. Please add props `collapasable: false` to your View
 
 ## 🎨 API's
@@ -167,12 +167,12 @@ AppTour.ShowSequence(appTourSequence)
 
 ## 🔧 Breaking Changes
 
-- [V0.0.4](https://github.com/prscX/react-native-app-tour/releases/tag/v0.0.4)
+- [V0.0.4](https://github.com/amir-hossein-karimi/react-native-app-tour/releases/tag/v0.0.4)
 
   - Generalized props across platforms @congnguyen91
   - Migrated License to Apache 2.0
 
-- [V0.0.10](https://github.com/prscX/react-native-app-tour/releases/tag/v0.0.10)
+- [V0.0.10](https://github.com/amir-hossein-karimi/react-native-app-tour/releases/tag/v0.0.10)
   - Added `order` as a mandatory property to each target
   - Each component which is to be rendered in the tour should have a `key` prop. It is mandatory.
 
@@ -183,7 +183,7 @@ AppTour.ShowSequence(appTourSequence)
 
 ## 🤔 How to contribute
 
-Have an idea? Found a bug? Please raise to [ISSUES](https://github.com/prscX/react-native-app-tour/issues).
+Have an idea? Found a bug? Please raise to [ISSUES](https://github.com/amir-hossein-karimi/react-native-app-tour/issues).
 Contributions are welcome and are greatly appreciated! Every little bit helps, and credit will always be given.
 
 ## 💫 Where is this library used?
@@ -194,7 +194,7 @@ If you are using this library in one of your projects, add it in this list below
 
 This library is provided under the Apache 2 License.
 
-RNAppTour @ [prscX](https://github.com/prscX)
+RNAppTour @ [amir-hossein-karimi](https://github.com/amir-hossein-karimi)
 
 ## 💖 Support my projects
 
@@ -203,12 +203,12 @@ I open-source almost everything I can, and I try to reply everyone needing help 
 However, if you get some profit from this or just want to encourage me to continue creating stuff, there are few ways you can do it:
 
 - Starring and sharing the projects you like 🚀
-- If you're feeling especially charitable, please follow [prscX](https://github.com/prscX) on GitHub.
+- If you're feeling especially charitable, please follow [amir-hossein-karimi](https://github.com/amir-hossein-karimi) on GitHub.
 
-  <a href="https://www.buymeacoffee.com/prscX" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: auto !important;width: auto !important;" ></a>
+  <a href="https://www.buymeacoffee.com/amir-hossein-karimi" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: auto !important;width: auto !important;" ></a>
 
   Thanks! ❤️
   <br/>
-  [prscX.github.io](https://prscx.github.io)
+  [amir-hossein-karimi.github.io](https://amir-hossein-karimi.github.io)
   <br/>
   </ Pranav >
--- android/src/main/java/ui/apptour/RNAppTourModule.java
+++ android/src/main/java/ui/apptour/RNAppTourModule.java
@@ -2,11 +2,16 @@ package ui.apptour;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.util.Log;
 import androidx.annotation.Nullable;
+import android.app.Dialog;
 import android.view.View;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.res.AssetManager;
 
@@ -14,10 +19,12 @@ import com.facebook.react.uimanager.UIManagerModule;
 import com.facebook.react.uimanager.UIBlock;
 import com.facebook.react.uimanager.NativeViewHierarchyManager;
 
+import com.facebook.common.internal.Objects;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.ReactContext;
 
 import com.facebook.react.bridge.Arguments;
@@ -60,6 +67,8 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
         final Activity activity = this.getCurrentActivity();
         final List<TapTarget> targetViews = new ArrayList<TapTarget>();
 
+        final Dialog dialog = new AlertDialog.Builder(activity).create();
+
         activity.runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -82,7 +91,7 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
                             targetViews.add(tapTarget);
                         }
 
-                        TapTargetSequence tapTargetSequence = new TapTargetSequence(activity).targets(targetViews);
+                        TapTargetSequence tapTargetSequence = new TapTargetSequence(dialog).targets(targetViews);
                         tapTargetSequence.considerOuterCircleCanceled(true);
 
                         tapTargetSequence.listener(new TapTargetSequence.Listener() {
@@ -92,6 +101,11 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
                                 params.putBoolean("finish", true);
 
                                 sendEvent(getReactApplicationContext(), "onFinishSequenceEvent", params);
+
+                                // dismiss dialog on finish
+                                if (dialog != null && dialog.isShowing()) {
+                                    dialog.dismiss();
+                                }
                             }
 
                             @Override
@@ -120,6 +134,7 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
     @ReactMethod
     public void ShowFor(final int view, final ReadableMap props, final Promise promise) {
         final Activity activity = getCurrentActivity();
+        final Dialog dialog = new AlertDialog.Builder(activity).create();
 
         activity.runOnUiThread(new Runnable() {
             @Override
@@ -139,7 +154,29 @@ public class RNAppTourModule extends ReactContextBaseJavaModule {
 
                         TapTarget targetView = generateTapTarget(refView, props);
 
-                        TapTargetView.showFor(activity, targetView, new TapTargetViewListener(props));
+                        TapTargetView.showFor(dialog, targetView, new TapTargetViewListener(props){
+                            @Override
+                            public void onTargetClick(TapTargetView view) {
+                                super.onTargetClick(view);
+
+                                WritableMap params = Arguments.createMap();
+                                sendEvent(getReactApplicationContext(), "onShowTargetClick", params);
+                            }
+
+                            @Override
+                            public void onOuterCircleClick(TapTargetView view) {
+                                super.onOuterCircleClick(view);
+
+                                WritableMap params = Arguments.createMap();
+                                sendEvent(getReactApplicationContext(), "onShowCircleClick", params);
+                            }
+
+                            @Override
+                            public void onTargetDismissed(TapTargetView view, boolean userInitiated) {
+                                WritableMap params = Arguments.createMap();
+                                sendEvent(getReactApplicationContext(), "onShowTargetDismissed", params);
+                            }
+                        });
                     }
                 });
             }
